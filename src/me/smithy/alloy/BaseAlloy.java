468204175e37c8a71901614b211cffdf925a6fda
IEatSushi <2klaus.4peter@gmail.com>
2021-07-10 22:40:28 +0200

    add recipe registraction skeleton

--- src/me/smithy/alloy/BaseAlloy.java
+++ src/me/smithy/alloy/BaseAlloy.java
@@ -1,5 +1,26 @@
 package me.smithy.alloy;
 
-public abstract class BaseAlloy {
+import org.bukkit.inventory.ItemStack;
 
+public abstract class BaseAlloy {
+	
+	private final String alloyName;
+	private final ItemStack mixedItem;
+	
+	public BaseAlloy(String alloyName, ItemStack mixedItem) {
+		this.alloyName = alloyName;
+		this.mixedItem = mixedItem.clone();
+	}
+	
+	public ItemStack getMixedItem() {
+		return mixedItem.clone();
+	}
+	
+	public String getAlloyName() {
+		return alloyName;
+	}
+	
+	public ItemStack getAlloyItem() {
+		return null;
+	}
 }
---
+++ src/me/smithy/alloy/RecipeRegistererer.java
@@ -0,0 +1,24 @@
+package me.smithy.alloy;
+
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapelessRecipe;
+import org.bukkit.inventory.SmithingRecipe;
+
+public class RecipeRegistererer {
+	
+	void registerAlloy(BaseAlloy alloy) {
+	
+	}
+	
+	ShapelessRecipe createAlloyRecipe(ItemStack mixedItem) {
+		return null;
+	}
+	
+	void createSmithingRecipes(BaseAlloy alloy) {
+	
+	}
+	
+	SmithingRecipe createSmithingRecipe(ItemStack toolItem, ItemStack alloyItem, ItemStack resultItem) {
+		return null;
+	}
+}
